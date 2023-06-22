55bb2770dfff1f113cc9e16f6e26b7504cda4fa2
Daniil Yakovlev <dyakovlev@tangl.cloud>
2023-06-22 23:07:10 +0500

    feature: add SimpleCSharpApp

---
+++ SimpleCSharpApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SimpleCSharpApp
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Вывести пользователю простое сообщение.
+            Console.WriteLine("******* Му First C# Арр *******"); 
+            Console.WriteLine("Hello World!"); 
+            Console.WriteLine();
+            // Ожидать нажатия клавиши <Enter>, прежде чем завершить работу.
+            Console.ReadKey();
+        }
+    }
+}
